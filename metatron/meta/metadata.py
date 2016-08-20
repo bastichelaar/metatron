57c650b84f6e4f8e5234d5f5629d90fdf39df0c0
Age Mooij <age.mooij@gmail.com>
2016-08-20 15:47:01 +0200

    Always add image, name, and version to the inspected metadata

--- metatron/meta/metadata.py
+++ metatron/meta/metadata.py
@@ -1,10 +1,25 @@
 # -*- coding: utf-8 -*-
 
+import re
+
 from pyhocon import ConfigFactory
 from .docker import dockerInspect
 
 def imageMetadata(image):
     inspected = dockerInspect(image)
     parsed = ConfigFactory.parse_string(inspected)
+    name, version = imageNameAndVersion(image)
+
+    parsed.put('meta.attributes.name', name)
+    parsed.put('meta.attributes.version', version)
+    parsed.put('meta.attributes.image', image)
 
     return parsed
+
+def imageNameAndVersion(image):
+    pattern = re.compile('(.+):(.+)')
+    matched = pattern.match(image)
+
+    # TODO: raise an error if matched is None
+
+    return matched.groups()
