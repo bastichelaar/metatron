3b6e1c4b210b319b2ef8808fa3518cbd441b897f
Age Mooij <age.mooij@gmail.com>
2016-08-22 16:58:07 +0200

    First workable version of meta2kube

--- metatron/meta/metadata.py
+++ metatron/meta/metadata.py
@@ -5,42 +5,66 @@ import re
 from pyhocon import *
 from .docker import dockerInspect
 
-def imageMetadata(image):
-    inspected = dockerInspect(image)
-    parsed = ConfigFactory.parse_string(inspected)
-    registry, project, version = imageNameAndVersion(image)
 
-    parsed.put('meta.attributes.image', image)
-    parsed.put('meta.attributes.project', project)
-    parsed.put('meta.attributes.version', version)
-    parsed.put('meta.attributes.registry', registry)
+class Metadata(object):
 
-    return parsed
+    def __init__(self, image):
+        def imageAttributes(image):
+            pattern = re.compile('(?:(.+)/)?(.+):(.+)')
+            matched = pattern.match(image)
+
+            # TODO: raise an error if matched is None
+
+            return matched.groups()
+
+        def parse(image):
+            inspected = dockerInspect(image)
+            parsed = ConfigFactory.parse_string(inspected)
+            registry, project, version = imageAttributes(image)
+
+            parsed.put('meta.attributes.image', image)
+            parsed.put('meta.attributes.project', project)
+            parsed.put('meta.attributes.version', version)
+            parsed.put('meta.attributes.registry', registry)
+
+            return parsed
+
+        self.__meta__ = parse(image)
 
 
-def imageNameAndVersion(image):
-    pattern = re.compile('(?:(.+)/)?(.+):(.+)')
-    matched = pattern.match(image)
+    # Add some ConfigTree/dict-like passthrough methods
 
-    # TODO: raise an error if matched is None
+    def items(self):
+        return self.__meta__.items()
 
-    return matched.groups()
+    def iterkeys(self):
+        return self.__meta__.iterkeys()
+
+    def __getitem__(self, key):
+        return self.__meta__[key]
 
 
-def flatten(config, prefix = '', keyFilter = lambda x: x):
-    def concat(prefix, suffix):
-        return suffix if prefix == '' else '%s.%s' % (prefix, suffix)
+    # Support for getting out a flattened set of filtered properties
 
-    list_of_results = [
-        flatten(value, concat(prefix, key), keyFilter) if isinstance(value, ConfigTree)
-            else [concat(prefix, key) + '=' + value]
+    def flatten(self, key_filter = lambda x: x):
+        return self.__flatten__(config = self.__meta__, key_filter = key_filter)
 
-        for key, value in config.items()
-        if keyFilter(key)
-    ]
+    @staticmethod
+    def __flatten__(config, prefix = '', key_filter = lambda x: x):
+        def concat(prefix, suffix):
+            return suffix if prefix == '' else '%s.%s' % (prefix, suffix)
 
-    return [item for result in list_of_results
-                 for item in result]
+        list_of_results = [
+            flatten(value, concat(prefix, key), key_filter) if isinstance(value, ConfigTree)
+                else [concat(prefix, key) + '=' + value]
+
+            for key, value in config.items()
+            if key_filter(key)
+        ]
+
+        return [item for result in list_of_results
+                     for item in result]
+
 
 
 # def flatten(config, prefix = '', results = [], keyFilter = lambda x: x):
--- metatron/meta2kube.py
+++ metatron/meta2kube.py
@@ -5,8 +5,6 @@ import click
 from metatron.meta import *
 from metatron.descriptors import *
 
-from pyhocon import *
-
 @click.command()
 # @click.option('--template', default="kubernetes", help='The deployment descriptor template to use.')
 @click.option('--pull', is_flag=True, default=False, help="Perform a docker pull before attempting to read image metadata. Defaults to False.")
@@ -16,24 +14,22 @@ def generate(pull, gcloud, image):
     """
     Generate Kubernetes deployment descriptors from docker IMAGE metadata.
     """
-    _, project, version = imageNameAndVersion(image) # TODO: handle errors
-
     if pull:
         dockerPull(image, gcloud) # TODO: handle errors
 
-    metadata = imageMetadata(image) # TODO: validate some actual metadata was found
+    metadata = Metadata(image)
 
-    click.echo('Project: %s' % project)
-    click.echo('Name:    %s' % metadata.get_string('meta.attributes.id'))
-    click.echo('Version: %s' % version)
-    click.echo('')
+    # click.echo('Project: %s' % metadata['meta.attributes.project'])
+    # click.echo('Name:    %s' % metadata['meta.attributes.id'])
+    # click.echo('Version: %s' % metadata['meta.attributes.version'])
+    # click.echo('')
 
     # click.echo('')
-    # flattened = flatten(metadata, keyFilter = lambda k: not(k.startswith('attributes')))
+    # flattened = metadata.flatten(key_filter = lambda k: not(k.startswith('attributes')))
     # for annotation in flattened:
     #     click.echo('%s' % annotation)
 
-    click.echo('Descriptor:\n\n%s' % descriptorFor('kubernetes', metadata))
+    click.echo(descriptorFor('kubernetes', metadata))
 
 
     # TODO:
@@ -42,9 +38,6 @@ def generate(pull, gcloud, image):
     # Maybe later:
     # - output format option? (yaml or json)
     # - specify a kubernetes label/annotation namespace? Of make this optional and have a default?
-
-
-
 
 
 def main():
